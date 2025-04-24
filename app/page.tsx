44f90a07ed0542917fefff230f2061c7ca2e1f61
Filip <filip.sostaric@protonmail.com>
2025-04-24 23:11:33 +0200

    Project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-	return (
-		<div>Home page</div>
-	)
-}
-export default Home
+  return <div>Home page</div>;
+};
+export default Home;
