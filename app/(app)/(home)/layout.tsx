bf31d6b5a09b5d9f27d1e576d3d3f5c2581518b4
Filip <filip.sostaric@protonmail.com>
2025-04-27 09:21:36 +0200

    small update

--- app/(app)/(home)/layout.tsx
+++ app/(app)/(home)/layout.tsx
@@ -1,16 +1,32 @@
+import { getPayload } from "payload";
 import React, { ReactNode } from "react";
 
 import { Footer } from "@/app/(app)/(home)/footer";
 import { Navbar } from "@/app/(app)/(home)/navbar";
+import { SearchFilters } from "@/app/(app)/(home)/search-filters";
+import configPromise from "@payload-config";
 
 type HomeLayoutProps = {
   children: ReactNode;
 };
 
-const HomeLayout = ({ children }: HomeLayoutProps) => {
+const HomeLayout = async ({ children }: HomeLayoutProps) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
   return (
     <div className={"flex flex-col min-h-screen"}>
       <Navbar />
+      <SearchFilters data={data} />
       <div className={"flex-1 bg-[#f4f4f0]"}>{children}</div>
       <Footer />
     </div>
--- app/(app)/(home)/page.tsx
+++ app/(app)/(home)/page.tsx
@@ -1,19 +1,6 @@
-import { getPayload } from "payload";
 import React from "react";
 
-import configPromise from "@payload-config";
-
-const payload = await getPayload({
-  config: configPromise,
-});
-const categories = await payload.find({
-  collection: "categories",
-});
-const Home = async () => {
-  return (
-    <pre>
-      <div>{JSON.stringify(categories, null, 2)}</div>;
-    </pre>
-  );
+const Home = () => {
+  return <div>Home page</div>;
 };
 export default Home;
