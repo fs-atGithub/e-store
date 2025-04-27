8b256345b3b3c795e0ef09dd12787f027e118b95
Filip <filip.sostaric@protonmail.com>
2025-04-27 12:10:40 +0200

    Search-filters

--- app/(app)/(home)/search-filters/categories.tsx
+++ app/(app)/(home)/search-filters/categories.tsx
@@ -2,22 +2,25 @@ import React from "react";
 
 import { CategoryDropdown } from "@/app/(app)/(home)/category-dropdown";
 import { Category } from "@/payload-types";
+
 type CategoriesProps = {
   data: any;
 };
 
 export const Categories = ({ data }: CategoriesProps) => {
   return (
-    <div>
-      {data.map((category: Category) => (
-        <div key={category.id}>
-          <CategoryDropdown
-            category={category}
-            isActive={false}
-            isNavigationHovered={false}
-          />
-        </div>
-      ))}
+    <div className={"relative w-full"}>
+      <div className={"flex items-center flex-nowrap"}>
+        {data.map((category: Category) => (
+          <div key={category.id}>
+            <CategoryDropdown
+              category={category}
+              isActive={false}
+              isNavigationHovered={false}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 import { Category } from "@/payload-types";
@@ -20,7 +21,7 @@ export const SubCategoryMenu = ({
   ) {
     return null;
   }
-  const backgroundColor = "#f5f5f5";
+  const backgroundColor = category.color || "#F5F5F5";
 
   return (
     <div
@@ -28,8 +29,25 @@ export const SubCategoryMenu = ({
       style={{ top: position.top, left: position.left }}
     >
       <div className={"h-3 w-60"} />
-      <div className={"w-60 text-black rounded-md overflow-hidden border"}>
-        <p>SubCategory</p>
+      <div
+        style={{ backgroundColor }}
+        className={
+          "w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0.1)] -translate-x-[2px] -translate-y-[2px]"
+        }
+      >
+        <div>
+          {category.subcategories?.map((subcategory: Category) => (
+            <Link
+              key={subcategory.slug}
+              href={"#"}
+              className={
+                "w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
+              }
+            >
+              {subcategory.name}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
